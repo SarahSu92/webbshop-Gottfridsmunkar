bea4638b0b3400bbb89ed341d305ec0e81ceb14a
SarahSu92 <sarah.sundberg@outlook.com>
2024-11-29 14:14:06 +0100

    Skapat en produktfil i JS.

---
+++ products.mjs
@@ -0,0 +1,135 @@
+//products
+
+const products = [
+  {
+    id: 0,
+    name: "Äppelmunk",
+    price: 15,
+    rating: 4,
+    amount: 0,
+    category: "Frukt",
+    img: {
+      url: "images/apple.png",
+      alt: "Munk med smak av äpple",
+    },
+  },
+
+  {
+    id: 1,
+    name: "Körsbärsmunk",
+    price: 19,
+    rating: 3,
+    amount: 0,
+    category: "Frukt",
+    img: {
+      url: "images/cherry.png",
+      alt: "Munk med smak av körsbär",
+    },
+  },
+
+  {
+    id: 2,
+    name: "Kaffemunk",
+    price: 19,
+    rating: 3,
+    amount: 0,
+    category: "Kryddor",
+    img: {
+      url: "images/coffee.png",
+      alt: "Munk med smak av kaffe",
+    },
+  },
+
+  {
+    id: 3,
+    name: "Kanelmunk",
+    price: 17,
+    rating: 3,
+    amount: 0,
+    category: "Kryddor",
+    img: {
+      url: "images/cinnamon.png",
+      alt: "Munk med smak av kanel",
+    },
+  },
+
+  {
+    id: 4,
+    name: "Lakritsmunk",
+    price: 15,
+    rating: 3,
+    amount: 0,
+    category: "Godis",
+    img: {
+      url: "images/licorice.png",
+      alt: "Munk med smak av lakrits",
+    },
+  },
+
+  {
+    id: 5,
+    name: "Chokaldmunk",
+    price: 15,
+    rating: 3,
+    amount: 0,
+    category: "Godis",
+    img: {
+      url: "images/chocolate.png",
+      alt: "Munk med smak av choklad",
+    },
+  },
+
+  {
+    id: 6,
+    name: "Polkagrismunk",
+    price: 18,
+    rating: 3,
+    amount: 0,
+    category: "Godis",
+    img: {
+      url: "images/candycane.png",
+      alt: "Munk med smak av polkagris",
+    },
+  },
+
+  {
+    id: 7,
+    name: "Cheesecake munk",
+    price: 15,
+    rating: 3,
+    amount: 0,
+    category: "Godis",
+    img: {
+      url: "images/creamy.png",
+      alt: "Munk med smak av cheesecake",
+    },
+  },
+
+  {
+    id: 8,
+    name: "Jordgubbsmunk",
+    price: 19,
+    rating: 3,
+    amount: 0,
+    category: "Frukt",
+    img: {
+      url: "images/strawberry.png",
+      alt: "Munk med smak av jordgubbe",
+    },
+  },
+
+  {
+    id: 9,
+    name: "Vaniljmunk",
+    price: 15,
+    rating: 3,
+    amount: 0,
+    category: "Kryddor",
+    img: {
+      url: "images/vanilla.png",
+      alt: "Munk med smak av vanlj",
+    },
+  },
+];
+
+export default products;
